0a0c8b0751a637147c4c741dc4705f4a9cf147c4
michaelsheridan23 <michael.sheridan23@gmail.com>
2023-12-30 17:25:26 -0500

    Corrected X-min Pos

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1823,7 +1823,7 @@
 #define Y_BED_SIZE 235
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS -2
+#define X_MIN_POS 0
 #define Y_MIN_POS -1
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
